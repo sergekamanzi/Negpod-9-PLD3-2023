ba6ffb90da4b87a1e100d55b4890097ae69ed5b9
sergekamanzi <s.kamanzi@alustudent.com>
2023-08-06 14:04:13 +0000

    001

---
+++ health.py
@@ -0,0 +1,8 @@
+mport tkinter as tk
+from tkinter import messagebox
+
+def register():
+    name = name_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    confirm_password = confirm_password_entry.get()
