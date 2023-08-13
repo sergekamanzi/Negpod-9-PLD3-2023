828d8f3692dac947a6d55381517ac8458d5e641a
renoir01 <r.kaze@alustudent.com>
2023-08-13 12:07:57 -0700

    project for healthcare issues

--- PLD4/telemedicine_project/healthcare_platform.py
+++ PLD4/telemedicine_project/healthcare_platform.py
@@ -5,4 +5,33 @@ from telemedicine_app.telemedicine_classes import TelemedicinePlatform  # Import
 
 if __name__ == "__main__":
     platform = TelemedicinePlatform()
-    platform.run()
+    while True:
+        platform.display_menu()
+        choice = input("Select an option: ")
+
+        if choice == '1':
+            platform.register_patient()
+        elif choice == '2':
+            platform.register_professional()
+        elif choice == '3':
+            platform.login()
+        elif choice == '4':
+            if platform.current_user:
+                platform.book_appointment()
+            else:
+                print("Please log in first.")
+        elif choice == '5':
+            platform.display_registered_patients()
+        elif choice == '6':
+            if platform.current_user:
+                platform.display_appointments()
+            else:
+                print("Please log in first.")
+        elif choice == '7':
+            platform.login_professional()
+        elif choice == '8':
+            platform.view_patient_appointments()
+        elif choice == '9':
+            break
+        else:
+            print("Invalid choice. Please try again.")
