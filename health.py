130fb603e95f92d8217d68e61e844c0cac75c21a
ruth iradukunda <r.iradukund@alustudent.com>
2023-08-13 12:44:59 -0700

    healthy

---
+++ health.py
@@ -0,0 +1,9 @@
+# Check if all fields are filled
+    if not name or not email or not password or not confirm_password:
+        messagebox.showerror("Error", "Please fill in all fields.")
+        return
+
+    # Check if passwords match
+    if password != confirm_password:
+        messagebox.showerror("Error", "Passwords do not match.")
+        return
