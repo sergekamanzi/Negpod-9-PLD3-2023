e24dc655799a5cfeb9ee5ae6096da1a20c0f5a09
sergekamanzi <s.kamanzi@alustudent.com>
2023-08-13 18:58:19 +0000

    p.classes

--- PLD4/telemedicine_project/telemedicine_app/patient_classes.py
+++ PLD4/telemedicine_project/telemedicine_app/patient_classes.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 class Patient:
-    def __init__(self, name, id_card, phone_number, email, age):
+    def __init__(self, name, id_card, phone_number):
         self.name = name
         self.id_card = id_card
         self.phone_number = phone_number
-        self.email = email
-        self.age = age
         self.role = "patient"
         self.appointments = []
 
